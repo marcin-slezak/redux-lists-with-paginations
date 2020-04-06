c6ae8c4dd430525e66e5ef5124b3e31d41ca0210
marcin slezak <marcin@wingedcode.com>
2020-04-07 00:38:08 +0200

    load data, createAsyncThunk for status tracking

--- store/ducks/lists-with-pagination.js
+++ store/ducks/lists-with-pagination.js
@@ -1,9 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createSelector, createAsyncThunk } from '@reduxjs/toolkit';
+const REQUEST_STATUS = {
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  FAILED: 'FAILED',
+};
 
-const addList = (state, action) => ({...state, lists: [...state.lists, action.payload]});
-const setRequestStatus = (state, action) => ({...state, lists})
+const addList = (state, action) => ({
+  ...state,
+  lists: [...state.lists, {
+    name: action.payload.name,
+    offset: 0,
+    limit: 0,
+    total: 0,
+    page: 0,
+    data: [],
+    url: !!action.payload.url && action.payload.url,
+    requestStatus: false
+  }]
+});
+
+const updateList = (state, action) => {
+  const { listName, data } = action.payload
+  const index = state.lists.findIndex(list => list.name === listName)
+
+  return {
+    ...state, lists: [
+      ...state.lists.slice(0, index),
+      { ...state.lists[index], ...data },
+      ...state.lists.slice(index + 1),
+    ]
+  };
+};
 
 
+const listSelector = listName => createSelector(state => state.lists, lists => lists.find(list => list.name === listName));
+
+const load = createAsyncThunk(
+  'lists-with-pagination/load',
+  async (listName, thunkAPI) => {
+    const url = thunkAPI.getState().listWithPagination.lists.find(list => list.name === listName);
+    const response = await fetch(url, {}); // todo offset limit etc
+    return await response.json();
+  }
+)
 
 const listWithPagination = createSlice({
   name: 'lists-with-pagination',
@@ -12,20 +51,27 @@ const listWithPagination = createSlice({
   },
   reducers: {
     addList,
-    setRequestStatus
+    updateList
+  },
+  extraReducers: {
+    [load.fulfilled]: (state, action) => {
+      const listName = action.meta.arg;
+      return updateList(state, {payload: {listName, data: {...action.payload, requestStatus: REQUEST_STATUS.SUCCESS}} });
+    },
+    [load.pending]: (state, action) => {
+      const listName = action.meta.arg;
+      return updateList(state, {payload: {listName, data: {requestStatus: REQUEST_STATUS.PENDING}} });
+    }
   },
 });
-
-listWithPagination.actions.getData = (listName) => async dispatch => {
-  dispatch(listWithPagination.actions.setRequestStatus(listName, 'in_progress'));
-  try{
-    const response = await fetch('', {});
-    dispatch(listWithPagination.actions.setRequestStatus(listName, 'success'));
-    const data = await response.json();
-    return data;
-  }catch(e){
-    dispatch(listWithPagination.actions.setRequestStatus(listName, 'failed'));
-  }
+listWithPagination.actions.load = load;
+listWithPagination.REQUEST_STATUS = REQUEST_STATUS;
+listWithPagination.actions.next = ({ listName }) => async (dispatch, getState) => {
+  // increase offset
+  // trigger load
 };
-
-export { listWithPagination };
+listWithPagination.actions.prev = ({ listName }) => async (dispatch, getState) => {
+  // decrese offset
+  // trigger load
+};
+export { listWithPagination, listSelector };
--- store/index.test.js
+++ store/index.test.js
@@ -1,7 +1,75 @@
 import { getStore } from '../store';
+import { listWithPagination, listSelector } from './ducks';
 
-test('example', () => {
+test('start with empty list', () => {
     const store = getStore();
-    console.log(store)
-    expect(true).toBe(true);
+    expect(store.getState().listWithPagination).toEqual({ lists: [] })
+})
+
+test('declare firsy list and list selector', async () => {
+    const store = getStore();
+    const list = {
+        name: 'files',
+        page: 0,
+        total: 0,
+        offset:0,
+        data: [],
+        limit: 0,
+        url: false,
+        requestStatus: false
+    };
+    await store.dispatch(listWithPagination.actions.addList({name: 'files'}));
+    expect(store.getState().listWithPagination).toEqual({
+        lists: [list]
+    });
+})
+
+test('empty list selector', async () => {
+    const store = getStore();
+    const filesListSelector = listSelector('files');
+    expect(filesListSelector(store.getState().listWithPagination)).toBe(undefined);
+})
+
+test('list selector', async () => {
+    const store = getStore();
+    const filesListSelector = listSelector('files');
+    const list = {
+        name: 'files',
+        page: 0,
+        total: 0,
+        offset:0,
+        data: [],
+        limit: 0,
+        url: false,
+        requestStatus: false
+    };
+    await store.dispatch(listWithPagination.actions.addList({name: 'files'}));
+    expect(filesListSelector(store.getState().listWithPagination)).toEqual(list);
+})
+
+test('load data', async () => {
+    
+    const serverResponse = {
+        total: 20,
+        data: [0,1,2,3,4,5,6,7,8,9],
+    };
+    global.fetch = jest.fn().mockResolvedValueOnce({ json: jest.fn().mockResolvedValueOnce({ ...serverResponse }) })
+    const store = getStore();
+    const nextUserStates = [];
+    store.subscribe(() => { nextUserStates.push(store.getState().listWithPagination.lists) });
+    await store.dispatch(listWithPagination.actions.addList({name: 'files', url: 'someTestUrl'}));
+    await store.dispatch(listWithPagination.actions.load('files'));
+    expect(store.getState().listWithPagination).toEqual({
+        lists: [{
+            name: 'files',
+            page: 0,
+            total: 20,
+            offset:0,
+            data: [0,1,2,3,4,5,6,7,8,9],
+            limit: 0,
+            url: 'someTestUrl',
+            requestStatus: listWithPagination.REQUEST_STATUS.SUCCESS
+        }]
+    });
+    console.log(nextUserStates)
 })
